bc6f986560811ef1e942070ea31e26b95f50ff8d
Jannatul Jami <jannatuljami37@gmail.com>
2024-07-15 13:11:43 -0400

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Scholar on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
